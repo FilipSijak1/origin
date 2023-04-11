cc5f9dc9bae76e250310f9245a024658d5b6070f
FilipSijak1 <126612710+FilipSijak1@users.noreply.github.com>
2023-04-11 20:18:30 +0200

    5.vjezba_Filip_Sijak

---
+++ Filip_Sijak_vjezba4/Utilities.py
@@ -0,0 +1,16 @@
+def unos_intervala(min, max):
+    while True:
+        try:
+            broj = int(input('Unesite cijeli broj: '))
+
+            if broj<min or broj>max:
+                raise Exception('Unesli ste broj koji nije u intervalu.')
+
+        except ValueError:
+            print('Unesli ste slovo umjesto broja.')
+
+        except Exception as e:
+            print(e)
+
+        else:
+            return broj
---
+++ Filip_Sijak_vjezba4/vjezba5.py
@@ -0,0 +1,29 @@
+from Utilities import unos_intervala
+from kolegij import unos_kolegija, ispis_svih_kolegija
+
+kolegiji = []
+
+running = True
+while running:
+    print('-'*20)
+    print('1. Unos novog kolegija')
+    print('2. Unos novog ispita')
+    print('3. Unos novog studenta')
+    print('4. Ispis kolegija')
+    print('5. Ispis ispita')
+    print('6. Ispis studenata')
+    print('7. Zaustavi program')
+    print('-' * 20)
+
+    akcija = unos_intervala(1, 7)
+
+    if akcija == 1:
+        kolegiji.append(unos_kolegija(len(kolegiji)+1))
+
+    elif akcija == 4:
+        ispis_svih_kolegija(kolegiji)
+        
+
+    if akcija == 7:
+        running = False
+
